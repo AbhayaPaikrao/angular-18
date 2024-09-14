57bf0295cbdb7d9b490d5e10b701d5add0df91b8
Abhaya <paikraoabhaya@gmail.com>
2024-09-14 15:52:49 +0530

    created shared module and  used in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
-import { CardComponent } from './shared/card/card.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { SharedModule } from './shared/shared.module';
+import { TaskModule } from './tasks/task.modules';
 
 
 @NgModule({
-    declarations:[AppComponent,HeaderComponent,UserComponent,CardComponent,TasksComponent,TaskComponent,NewTaskComponent],
+    declarations:[AppComponent,HeaderComponent,UserComponent],
     bootstrap : [AppComponent],
-    imports:[BrowserModule, FormsModule]
+    imports:[BrowserModule, FormsModule,SharedModule,TaskModule]
 })
 export class AppModule{}
---
+++ src/app/tasks/task.modules.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { TaskComponent } from "./task/task.component";
+import { TasksComponent } from "./tasks.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { SharedModule } from "../shared/shared.module";
+import { CommonEngine } from "@angular/ssr";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+    declarations:[TaskComponent,TasksComponent,NewTaskComponent],
+    exports: [TasksComponent],
+    imports:[BrowserModule,CommonModule,SharedModule]
+})
+export class TaskModule{}
